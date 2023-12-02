fa591292c76c738a4b2f0f9d609283d3c46d472f
Naoise <naoisedaly101@gmail.com>
2023-12-02 20:15:49 +0000

    Modularised the ass3 package into the "cs3318ass3v1Mod" module

---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module cs3318ass3v1Mod {
+    //java awt Color objects are returned by some methods of ColourTable so transitive dependency
+    requires transitive java.desktop;
+    exports ass3;
+}
